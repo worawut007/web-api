aad547508181e80118f3ac1fff4293cb96cca14d
Tiw.thitipong <mr.thitipongtiw@gmail.com>
2017-11-30 23:03:22 +0700

    split name and surname

--- contact/index.js
+++ contact/index.js
@@ -4,50 +4,40 @@ const bodyParser = require('body-parser')
 app.use(bodyParser.json())
 
 var contactList = [
-    {id: 0, name: 'Ned Stark', email: 'ned@winterfell.com', phone: '123-456-7890', url: 'www.google.com', notes: 'Winter is coming.'},
-    {id: 1, name: 'Theon Greyjoy', email: 'tgreyjoy@winterfell.com', phone: '123-456-7890', url: 'www.google.com', notes: 'Reluctant to pay iron price.'},
-    {id: 2, name: 'Samwell Tarly', email: 'starly@castleblack.com', phone: '123-456-7890', url: 'www.google.com', notes: 'Loyal brother of the watch.'},
-    {id: 3, name: 'Jon Snow', email: 'jsnow@castleblack.com', phone: '123-456-7890', url: 'www.google.com', notes: 'Knows nothing.'},
-    {id: 4, name: 'Arya Stark', email: 'waterdancer@winterfell.com', phone: '123-456-7890', url: 'www.google.com', notes: 'Has a list of names.'},
-    {id: 5, name: 'Jora Mormont', email: 'khaleesifan100@gmail.com', phone: '123-456-7890', url: 'www.google.com', notes: 'Lost in the friend-zone.'},
-    {id: 6, name: 'Tyrion Lannister', email: 'tyrion@lannister.com', phone: '123-456-7890', url: 'www.google.com', notes: 'Currently drunk.'},
-    {id: 7, name: 'Stannis Baratheon', email: 'onetrueking@dragonstone.com', phone: '123-456-7890', url: 'www.google.com', notes: 'Nobody expects the Stannish inquisition.'},
-    {id: 8, name: 'Hodor', email: 'hodor@hodor.com', phone: '123-456-7890', url: 'www.google.com', notes: 'Hodor? Hodor... Hodor!'},
-    {id: 9, name: 'Margaery Tyrell', email: 'mtyrell@highgarden.com', phone: '123-456-7890', url: 'www.google.com', notes: 'Keeper of kings.'},
-    {id: 10, name: 'Brienne of Tarth', email: 'oathkeeper@gmail.com', phone: '123-456-7890', url: 'www.google.com', notes: 'Do not cross her.'},
-    {id: 11, name: 'Petyr Baelish', email: 'petyr@baelishindustries.com', phone: '123-456-7890', url: 'www.google.com', notes: 'Do not trust anyone.'},
+    {id: 0, name: 'Ned', surname: 'Stark', email: 'ned@winterfell.com', phone: '123-456-7890', url: 'www.google.com', notes: 'Winter is coming.'},
+    {id: 1, name: 'Theon ', surname: 'Greyjoy', email: 'tgreyjoy@winterfell.com', phone: '123-456-7890', url: 'www.google.com', notes: 'Reluctant to pay iron price.'},
+    {id: 2, name: 'Samwell ', surname: 'Tarly', email: 'starly@castleblack.com', phone: '123-456-7890', url: 'www.google.com', notes: 'Loyal brother of the watch.'},
+    {id: 3, name: 'Jon ', surname: 'Snow', email: 'jsnow@castleblack.com', phone: '123-456-7890', url: 'www.google.com', notes: 'Knows nothing.'},
+    {id: 4, name: 'Arya ', surname: 'Stark', email: 'waterdancer@winterfell.com', phone: '123-456-7890', url: 'www.google.com', notes: 'Has a list of names.'},
+    {id: 5, name: 'Jora ', surname: 'Mormont', email: 'khaleesifan100@gmail.com', phone: '123-456-7890', url: 'www.google.com', notes: 'Lost in the friend-zone.'},
+    {id: 6, name: 'Tyrion ', surname: 'Lannister', email: 'tyrion@lannister.com', phone: '123-456-7890', url: 'www.google.com', notes: 'Currently drunk.'},
+    {id: 7, name: 'Stannis ', surname: 'Baratheon', email: 'onetrueking@dragonstone.com', phone: '123-456-7890', url: 'www.google.com', notes: 'Nobody expects the Stannish inquisition.'},
+    {id: 8, name: 'Hodor', surname: '', email: 'hodor@hodor.com', phone: '123-456-7890', url: 'www.google.com', notes: 'Hodor? Hodor... Hodor!'},
+    {id: 9, name: 'Margaery ', surname: 'Tyrell', email: 'mtyrell@highgarden.com', phone: '123-456-7890', url: 'www.google.com', notes: 'Keeper of kings.'},
+    {id: 10, name: 'Brienne ', surname: 'of Tarth', email: 'oathkeeper@gmail.com', phone: '123-456-7890', url: 'www.google.com', notes: 'Do not cross her.'},
+    {id: 11, name: 'Petyr ', surname: 'Baelish', email: 'petyr@baelishindustries.com', phone: '123-456-7890', url: 'www.google.com', notes: 'Do not trust anyone.'},
   ]
 
-
-  app.get('/contacts',(req,res) => {
-      res.json(contactList)
-  })    
-  app.post('/contacts',(req,res) => {
-    contactList.push(req.body)
-  })
-
-  module.exports=app
-
-
-  app.delete('/contacts/:id',(req,res)=>{
-    console.log(req.params)
- 
-        contactList.splice(req.params.id,1)
+app.get('/contacts', (req, res) => {
     res.json(contactList)
-    
-  })
-  
-  app.get('/contacts', (req, res) => res.json(contactList))
-  app.get('/contacts/:id', (req, res) => {
-    let d = req.params.id  
-    res.json(contactList[d])
 })
-
-  app.put('/contacts/:id',(req,res)=>{
+app.get('/contacts/:id', (req, res) => {
+  let d = req.params.id  
+  res.json(contactList[d])
+})
+app.post('/contacts',(req,res) => {
+    contactList.push(req.body)
+})
+app.delete('/contacts/:id',(req,res)=>{
+    console.log(req.params)
+    contactList.splice(req.params.id,1)
+    res.json(contactList) 
+})
+app.put('/contacts/:id',(req,res)=>{
     contactList[req.params.id]=req.body
-  })
+})
  
-  module.exports=app
+module.exports=app
 
  
 
